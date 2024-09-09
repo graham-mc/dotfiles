d3145910a90ea1b8dfeb4cc013c6c84a7886b811
Graham <3437644+graham-mc@users.noreply.github.com>
2024-09-09 21:22:11 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,20 @@
+; Alter Emacs Defaults
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'deeper-blue t)
-(set-face-attribute 'default nil :height 110)
 (setq inhibit-startup-screen t)
 (blink-cursor-mode 0)
+(setq ring-bell-function 'ignore)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(cond
+  ((eq system-type 'darwin)
+    (set-face-attribute 'default nil :height 150))
+  ((eq system-type 'gnu/linux)
+    (set-face-attribute 'default nil :height 110)))
 
+
+; Org Mode Configs
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-(setq org-todo-keywords '((sequence "TODO" "DOING" "DONE")))
+(setq org-todo-keywords '((sequence "TODO" "WAITING" "DOING" "DONE")))

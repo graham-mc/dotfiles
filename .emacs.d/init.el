b7afa892adfb4e3ec951c36e1ba95ab0b41645c1
Graham <3437644+graham-mc@users.noreply.github.com>
2024-11-10 15:14:29 +0000

    include crafted emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,31 +1,41 @@
-; Defaults
+;;; Crafted Emacs
+
+;; This assumes you cloned Crafted Emacs in you home directory.
+(load "~/crafted-emacs/modules/crafted-init-config")
+(package-install-selected-packages :noconfirm)
+(require 'crafted-defaults-config)
+(require 'crafted-osx-config)
+(require 'crafted-completion-packages)
+(package-install-selected-packages :noconfirm)
+(require 'crafted-completion-config)
+
+;;; Personal Defaults
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'deeper-blue t)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (blink-cursor-mode 0)
-(winner-mode 1)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (add-hook 'text-mode-hook 'auto-fill-mode)
 (setq ring-bell-function 'ignore)
 (setq inhibit-startup-screen t)
 (setq suggest-key-bindings 5)
 (cond
-  ((eq system-type 'darwin)
-    (set-face-attribute 'default nil :height 150))
-  ((eq system-type 'gnu/linux)
-    (set-face-attribute 'default nil :height 110)))
-(cond
-  ((eq system-type 'darwin)
-    (setq ns-right-alternate-modifier (quote none))))
+ ((eq system-type 'darwin)
+  (set-face-attribute 'default nil :height 150))
+ ((eq system-type 'gnu/linux)
+  (set-face-attribute 'default nil :height 110)))
 
-; Ace Window
+;;; Personal Packages
+
+;; Ace Window
 (use-package ace-window
   :ensure t)
 (global-set-key (kbd "M-o") 'ace-window)
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 
-; Org
+;; Org
 (use-package org
   :ensure t)
 (add-hook 'org-mode-hook 'auto-fill-mode)
@@ -36,17 +46,11 @@
 (setq org-todo-keyword-faces
       '(("DOING" . (:inherit warning :weight bold))))
 
-; Ivy
-(use-package counsel
-  :ensure t)
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-
-; Magit
+;; Magit
 (use-package magit
   :ensure t)
 
-; Rust
+;; Rust
 (use-package rust-mode
   :ensure t)
+
